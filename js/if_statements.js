6f5f1eec992a36572f511656ddbbc95b6b1fcf76
dries-melis <dries.melis@melweb.org>
2020-08-26 11:43:48 +0200

    in the middle of the control flow

--- js/if_statements.js
+++ js/if_statements.js
@@ -8,3 +8,13 @@ if(lenght > 172)
     console.log('You are not that big (yet)');
 } 
 
+
+
+const password = 'pass';
+
+if(password.length >= 8)
+{
+    console.log('This password is long enough');
+} else
+{
+    console.log('This password isn\'t long enough');
