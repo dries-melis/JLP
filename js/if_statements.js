cffe5e0058d26790e3cd3bfdf469b3cea8bafa99
dries-melis <dries.melis@melweb.org>
2020-08-26 22:07:26 +0200

    worked on if_statements.js

--- js/if_statements.js
+++ js/if_statements.js
@@ -10,12 +10,39 @@ if(lenght > 172)
 
 
 
-const password = 'pass';
+const password = 'password';
 
-if(password.length >= 8)
+
+if(password.length >= 12)
 {
-    console.log('This password is long enough');
-} else
+    console.log('This password is Strong enough');
+}
+else if(password.length >= 8)
+{
+    console.log('This password is strong enough but it can be stronger ;)');
+} 
+else
 {
     console.log('This password isn\'t long enough');
+}
+
+
+// Logical operators OR || AND &&
+
+let email = 'test.mail@testmail.com';
+
+if(email.includes('@') && email.includes('.'))
+{
+    console.log(` ${email} is a correct email adress!!!`);
+}
+else if(email.includes('@') && ! email.includes('.'))
+{
+    console.log('There must be a period (.) in a email adress!!');
+} 
+else if(! email.includes('@') && email.includes('.'))
+{
+    console.log('There must be a @ in a email adress!!');
+} else
+{
+    console.log('This isn\'t a corect email adress!!')
 }
