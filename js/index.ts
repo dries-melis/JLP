358db4926a9a8eb9e28b5af94b06605c6516c8ea
dries-melis <dries.melis@melweb.org>
2020-08-29 22:31:22 +0200

    created a project to push on buttons

--- js/index.ts
+++ js/index.ts
@@ -1,4 +1,5 @@
+
 // if there is clicked on the div then he's gonna look if the click was on the button
 $('div.outer-container').on('click', 'button', function () {
-    $("div.outer-container").append('<button class="btn btn-secondary flexbutton">Push Me!!</button>');
+  $("div.outer-container").append('<button class="btn btn-secondary flexbutton">Push Me!!</button>');
 });
