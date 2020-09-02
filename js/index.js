e76285b14ae82d92ef0da311f548b97bbd2f66f0
dries-melis <dries.melis@melweb.org>
2020-09-02 18:00:58 +0200

    added a service worker"

--- js/index.js
+++ js/index.js
@@ -1,3 +1,13 @@
+if ("serviceWorker" in navigator){
+    navigator.serviceWorker.register("sw.js").then(registration => {
+        console.log("SW Registered!");
+        console.log(registration);
+    }).catch(error => {
+        console.log("Sw Registration Failed");
+        console.log(error);
+    });
+}
+
 // if there is clicked on the div then he's gonna look if the click was on the button
 $('div.outer-container').on('click', 'button', function () {
     $("div.outer-container").append('<button class="btn btn-secondary flexbutton">Push Me!!</button>');
--- sw.js
+++ sw.js
@@ -1,48 +1,15 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("static").then(cache => {
+      return cache.addAll(["./", "./css/main.css", "./js/index.js", "./images/logo.png", "./manifest.webmanifest"]);
+    })
+  );
+})
 
-const cacheName = 'news-v1';
-const staticAssets = [
-  './',
-  './index.html',
-  './css/main.css',
-  './js/index.js',
-
-];
-
-self.addEventListener('install', async e => {
-  const cache = await caches.open(cacheName);
-  await cache.addAll(staticAssets);
-  return self.skipWaiting();
-});
-
-self.addEventListener('activate', e => {
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', async e => {
-  const req = e.request;
-  const url = new URL(req.url);
-
-  if (url.origin === location.origin) {
-    e.respondWith(cacheFirst(req));
-  } else {
-    e.respondWith(networkAndCache(req));
-  }
-});
-
-async function cacheFirst(req) {
-  const cache = await caches.open(cacheName);
-  const cached = await cache.match(req);
-  return cached || fetch(req);
-}
-
-async function networkAndCache(req) {
-  const cache = await caches.open(cacheName);
-  try {
-    const fresh = await fetch(req);
-    await cache.put(req, fresh.clone());
-    return fresh;
-  } catch (e) {
-    const cached = await cache.match(req);
-    return cached;
-  }
-}
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  )
+})
